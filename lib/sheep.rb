31d45be49606c43b3f536d06f9c8243413727c03
Elliott Hilaire <elliott.hilaire@gmail.com>
2016-10-01 11:36:50 +0100

    Clean up sheep class

--- lib/sheep.rb
+++ lib/sheep.rb
@@ -5,55 +5,44 @@ module Sheeps
       @window = window
       @x = x
       @y = y
-      @font = Gosu::Font.new(@window, Gosu::default_font_name, 16)
-
-      image = ['media/sheep_1.png','media/sheep_2.png','media/sheep_3.png'].sample
-      @image = Gosu::Image.new(@window, image, false)
+      @image = Gosu::Image.new(@window, random_sheep_image, false)
       @direction = 0
     end
 
     def move
-      if Gosu::distance(@x, @y, @window.herder.x, @window.herder.y) < 25
-        theta = Gosu::angle(@x, @y, @window.herder.x, @window.herder.y)
-
-        @x -= Gosu::offset_x(theta, 1)
-        @y -= Gosu::offset_y(theta, 1)
-        @direction = theta
+      if Gosu::distance(@x, @y, herder.x, herder.y) < 25
+        @direction = Gosu::angle(@x, @y, herder.x, herder.y)
+        @x -= Gosu::offset_x(@direction, 1)
+        @y -= Gosu::offset_y(@direction, 1)
       end
 
-      if Gosu::distance(@x, @y, @window.dog.x, @window.dog.y) < 120
-        theta = Gosu::angle(@x, @y, @window.dog.x, @window.dog.y)
-
-        @x -= Gosu::offset_x(theta, 3)
-        @y -= Gosu::offset_y(theta, 3)
-        @direction = theta
+      if Gosu::distance(@x, @y, dog.x, dog.y) < 120
+        @direction = Gosu::angle(@x, @y, dog.x, dog.y)
+        @x -= Gosu::offset_x(@direction, 3)
+        @y -= Gosu::offset_y(@direction, 3)
       end
 
-      @window.sheep.reject{ |sheep| sheep == self }.each do |sheep|
+      other_sheep.each do |sheep|
         distance_to_other_sheep = Gosu::distance(@x, @y, sheep.x, sheep.y)
 
         if distance_to_other_sheep < 50
-           theta = Gosu::angle(@x, @y, sheep.x, sheep.y)
-           @x -= Gosu::offset_x(theta, 0.7)
-           @y -= Gosu::offset_y(theta, 0.7)
-           @direction = theta
+           @direction = Gosu::angle(@x, @y, sheep.x, sheep.y)
+           @x -= Gosu::offset_x(@direction, 0.7)
+           @y -= Gosu::offset_y(@direction, 0.7)
         end
 
         if distance_to_other_sheep > 100 && distance_to_other_sheep < 200
-          theta = Gosu::angle(@x, @y, sheep.x, sheep.y)
-          @x += Gosu::offset_x(theta, 0.5)
-          @y += Gosu::offset_y(theta, 0.5)
-          @direction = theta
+          @direction = Gosu::angle(@x, @y, sheep.x, sheep.y)
+          @x += Gosu::offset_x(@direction, 0.5)
+          @y += Gosu::offset_y(@direction, 0.5)
         end
       end
 
-      @window.pen.fence.each do |fence_point|
+      pen.fence.each do |fence_point|
         if Gosu::distance(@x, @y, fence_point[:x], fence_point[:y]) < 3
-          theta = Gosu::angle(@x, @y, @window.herder.x, @window.herder.y)
-
-          @x -= Gosu::offset_x(theta, 3)
-          @y -= Gosu::offset_y(theta, 3)
-          @direction = theta
+          @direction = Gosu::angle(@x, @y, fence_point[:x], fence_point[:y])
+          @x -= Gosu::offset_x(@direction, 3)
+          @y -= Gosu::offset_y(@direction, 3)
         end
       end
     end
@@ -66,6 +55,28 @@ module Sheeps
       else
         @image.draw(@x + offset_x, @y - offset_y, 0, -1, 1)
       end
+    end
+
+    private
+
+    def herder
+      @window.herder
+    end
+
+    def dog
+      @window.dog
+    end
+
+    def other_sheep
+      @window.sheep.reject{ |sheep| sheep == self }
+    end
+
+    def pen
+      @window.pen
+    end
+
+    def random_sheep_image
+      ['media/sheep_1.png','media/sheep_2.png','media/sheep_3.png'].sample
     end
   end
 end
